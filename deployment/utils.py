b64956a1a0aad1fa0227918dc3b8354885325544
Mat Lee <ml7@uw.edu>
2016-09-21 19:07:04 -0700

    Python 3 print fix.

--- deployment/utils.py
+++ deployment/utils.py
@@ -3,13 +3,13 @@ import subprocess
 
 def rsync(user_name, host_name, source_dir, target_base_dir):
     command = ['rsync', '-azv', source_dir, '{}@{}:{}'.format(user_name, host_name, target_base_dir)]
-    print ' '.join(command)
+    print(' '.join(command))
 
     return subprocess.call(command)
 
 
 def remoteMkdir(user_name, host_name, target_base_dir):
     command = ['ssh', '{}@{}'.format(user_name, host_name), 'mkdir -p {}'.format(target_base_dir.rstrip('/'))]
-    print ' '.join(command)
+    print(' '.join(command))
 
     return subprocess.call(command)
