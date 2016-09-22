36ed6b10818c666c41774471aa91d03f814ef378
Mat Lee <ml7@uw.edu>
2016-09-21 19:21:55 -0700

    Deployment fix, getting rid of extra nested directories created on code push...

--- deployment/pushAndRefresh.py
+++ deployment/pushAndRefresh.py
@@ -10,9 +10,8 @@ if __name__ == '__main__':
     user_name = 'ubuntu'
     host_name = 'ec2-52-35-185-162.us-west-2.compute.amazonaws.com'
     source_dir = BASE_DIR
-    target_base_dir = '/home/ubuntu/SfccPro'
+    target_parent_dir = '/home/ubuntu'
 
-    utils.remoteMkdir(user_name, host_name, target_base_dir)
-    utils.rsync(user_name, host_name, source_dir, target_base_dir, ignore=['db.sqllite3'])
+    utils.rsync(user_name, host_name, source_dir, target_parent_dir, ignore=['db.sqllite3'])
 
     # TODO: Add start/restart
--- deployment/utils.py
+++ deployment/utils.py
@@ -1,8 +1,8 @@
 import subprocess
 
 
-def rsync(user_name, host_name, source_dir, target_base_dir, ignore=[]):
-    command = ['rsync', '-azv', source_dir, '{}@{}:{}'.format(user_name, host_name, target_base_dir)]
+def rsync(user_name, host_name, source_dir, target_parent_dir, ignore=[]):
+    command = ['rsync', '-azv', source_dir, '{}@{}:{}'.format(user_name, host_name, target_parent_dir)]
     for file_or_dir in ignore:
         command.insert(2, '--exclude={}'.format(file_or_dir))
     print(' '.join(command))
@@ -10,8 +10,8 @@ def rsync(user_name, host_name, source_dir, target_base_dir, ignore=[]):
     return subprocess.call(command)
 
 
-def remoteMkdir(user_name, host_name, target_base_dir):
-    command = ['ssh', '{}@{}'.format(user_name, host_name), 'mkdir -p {}'.format(target_base_dir.rstrip('/'))]
+def remoteMkdir(user_name, host_name, target_dir):
+    command = ['ssh', '{}@{}'.format(user_name, host_name), 'mkdir -p {}'.format(target_dir.rstrip('/'))]
     print(' '.join(command))
 
     return subprocess.call(command)
