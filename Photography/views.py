0c4f3dbcb70832bf596d0b59d9beffb521e6f460
Mat Lee <ml7@uw.edu>
2016-09-23 23:34:53 -0700

    Adding view function for listing all photos in S3.

--- Photography/views.py
+++ Photography/views.py
@@ -1,3 +1,23 @@
+import os
+
+import boto
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
 
 # Create your views here.
+
+
+class Gallery(TemplateView):
+
+    def get(self, request):
+        s3 = boto.s3.connect_to_region('us-west-2')
+
+        bucket = s3.get_bucket('seattlefcc-photos')
+
+        presigned_urls = [
+            s3.generate_url(method='GET', bucket=bucket.name, key=key.name, expires_in=20000000 , force_http=True)
+            for key in bucket.list(prefix='gallery')
+        ][1:]
+
+        return render(request, 'Photography/templates/gallery.html', context={'presigned_urls': presigned_urls})
