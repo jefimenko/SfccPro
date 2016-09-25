1c99c251bc98a8eaf646858dba505f3a94b28853
Mat Lee <ml7@uw.edu>
2016-09-25 09:47:49 -0700

    Adding bare domain name to allowed hosts.

--- SfccPro/settings.py
+++ SfccPro/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import getpass
 import os
-import socket
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -29,7 +28,7 @@ IS_PRODUCTION = os.environ.get('SERVERTYPE') == 'production'
 # SECURITY WARNING: don't run with debug turned on in production!
 if IS_PRODUCTION:
     DEBUG = False
-    ALLOWED_HOSTS = [socket.gethostname()]
+    ALLOWED_HOSTS = ['seattlefcc.pro', 'www.seattlefcc.pro']
 else:
     DEBUG = True
     ALLOWED_HOSTS = []
