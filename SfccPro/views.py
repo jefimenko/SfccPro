17875cc025825d387b041eb716fae072c262551b
Mat Lee <ml7@uw.edu>
2016-09-24 22:56:43 -0700

    Template reference fixes.

--- SfccPro/views.py
+++ SfccPro/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 from django.template import RequestContext
 from django.views.generic import TemplateView
 
 
 class Home(TemplateView):
+
     def get(self, request):
-        return render_to_response('SfccPro/templates/index.html', context_instance=RequestContext(request))
+        banner_image_url = 'http://seattlefcc.pro.s3-us-west-2.amazonaws.com/24.jpg?Signature=omujZMy%2BlvKfxBKvQRAziIDVxDM%3D&Expires=1475191159&AWSAccessKeyId=AKIAI5J3BFO5Q334A53Q'
+
+        return render(
+            request=request,
+            template_name='SfccPro/templates/index.html',
+            context={'banner_image_url': banner_image_url}
+        )
