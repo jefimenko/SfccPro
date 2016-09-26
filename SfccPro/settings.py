af7e4aa45d9ef766633f7e30ddc4f7b9bab8fe02
Mat Lee <ml7@uw.edu>
2016-09-26 16:43:23 -0700

    Adding /static/js to settings static file directories.

--- SfccPro/settings.py
+++ SfccPro/settings.py
@@ -191,6 +191,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
     ('css', os.path.join(STATIC_ROOT, 'css')),
+    ('js', os.path.join(STATIC_ROOT, 'js')),
 
     ('Photography', os.path.join(BASE_DIR, 'Photography/static')),
     ('SfccPro', os.path.join(BASE_DIR, 'SfccPro/static')),
